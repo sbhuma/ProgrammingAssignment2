2f38f3eb9546b47e20217b8503348e15c298c8e8
sriram bhuma <sbhuma.next@gmail.com>
2016-11-27 21:55:50 -0500

    Aftrer adding meat for 2 pre-defined functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below functions demonstrate how assign a value to an object in an 
+## environment that is different from the current environment
 
-## Write a short comment describing this function
+## Below function get & set value of the mstrix and also  get & set for inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Following function calculates the inverse of the given matrix created with 
+## above function. If the inverse already exists then it gets the inverse from
+## the cache and skip calculation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m		
 }
